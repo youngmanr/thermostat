842a381793ebf9c714887df3e509c2dd990233b1
Robert Youngman <rob_youngman@yahoo.com>
2015-11-11 14:56:22 +0000

    increase temperature test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,8 +1,17 @@
 describe('thermostat', function(){
-  var thermostat
-})
+  var thermostat;
 
-it('thermostat starts at 20 degress', function(){
-  thermostat.temperature;
-  expect(thermostat.temperature).toEqual(20);
+  beforeEach(function() {
+    thermostat = new Thermostat();
+  });
+
+  it('thermostat starts at 20 degress', function(){
+    thermostat.temperature;
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  it('increases the temperature with the up button', function(){
+    thermostat.increaseTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  });
 });
---
+++ src/thermostat.js
@@ -0,0 +1,7 @@
+function Thermostat() {
+  this.temperature = 20;
+}
+
+Thermostat.prototype.getCurrentTemperature = function() {
+  return this.temperature;
+};
