fe7494b7a6a044ac2fa3d312677e74a8990568bd
jamierose <jrose111284@gmail.com>
2015-11-11 12:33:59 +0000

    starts a 20 degress test

---
+++ spec/thermostatSpec.js
@@ -0,0 +1,8 @@
+describe('thermostat', function(){
+  var thermostat
+})
+
+it('thermostat starts at 20 degress', function(){
+  thermostat.temperature;
+  expect(thermostat.temperature).toEqual(20);
+});
