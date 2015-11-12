c61bad88d8b0ebf8bb156b708532ebdc933bd99a
Norm Fasey <norm@fasey.net>
2015-11-12 12:25:11 +0000

    Up button working

--- src/thermostat.js
+++ src/thermostat.js
@@ -11,20 +11,20 @@ Thermostat.prototype.increaseTemperature = function() {
   var maxTemp = (this.isPowerSavingModeOn()) ? 25 : 32;
   if (this.temperature < maxTemp) {
     this.temperature += 1;
-  };
+  }
   return this.temperature;
 };
 
 Thermostat.prototype.decreaseTemperature = function() {
   if (this.temperature > 10) {
     this.temperature -= 1;
-  };
+  }
   return this.temperature;
 };
 
 Thermostat.prototype.isPowerSavingModeOn = function() {
   return this.powerSavingMode;
-}
+};
 
 Thermostat.prototype.switchPowerSavingModeOff = function() {
   this.powerSavingMode = false;
@@ -38,9 +38,9 @@ Thermostat.prototype.resetTemperature = function() {
 Thermostat.prototype.displayColour = function() {
     if (this.temperature < 18) {
       return 'green';
-    };
+    }
     if (this.temperature < 25) {
       return 'yellow';
-    };
+    }
       return 'red';
 };
